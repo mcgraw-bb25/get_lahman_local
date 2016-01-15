a6d61d9b8f7d10780a21d23fe976425dbb9ad416
matt mcgraw <matthew.mcgraw@gmail.com>
2016-01-14 20:46:53 -0500

    changing create script to add batting

--- app/models/create_all_tables.py
+++ app/models/create_all_tables.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
+'''
+from ../app 
+    $ python -m models.create_all_tables --create_tables Y
+    $ python -m models.create_all_tables --drop_tables Y
+'''
 import argparse
 
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
 
 from data_config import DATABASE_URI
 
@@ -12,7 +17,8 @@ engine = create_engine(DATABASE_URI, echo=True)
 
 master = Table("master", metadata,
     Column('id', Integer, primary_key=True),
-    Column('player_id', String(), unique=True, nullable=False),
+    Column('player_id', String(), unique=True,
+                                  nullable=False),
     Column('birth_year', Integer),
     Column('birth_month', Integer),
     Column('birth_day', Integer),
@@ -39,17 +45,40 @@ master = Table("master", metadata,
     )
 
 
-if __name__ == "__main__":
-    '''
-    from ../app 
-        $ python -m models.create_all_tables --create_tables Y
-        $ python -m models.create_all_tables --drop_tables Y
-    '''
+batting = Table("batting", metadata,
+    Column('id', Integer, primary_key=True),
+    Column('player_id', String(), ForeignKey("master.player_id"),
+                                 nullable=False),
+    Column('year_id', Integer),
+    Column('stint', Integer),
+    Column('team_id', String()),
+    Column('lg_id', String()),
+    Column('_G', Integer),
+    Column('_AB', Integer),
+    Column('_R', Integer),
+    Column('_H', Integer),
+    Column('_2B', Integer),
+    Column('_3B', Integer),
+    Column('_HR', Integer),
+    Column('_RBI', Integer),
+    Column('_SB', Integer),
+    Column('_CS', Integer),
+    Column('_BB', Integer),
+    Column('_SO', Integer),
+    Column('_IBB', Integer),
+    Column('_HBP', Integer),
+    Column('_SH', Integer),
+    Column('_SF', Integer),
+    Column('_GIDP', Integer)
+    )
 
-    table_list = []
+if __name__ == "__main__":
 
     ### append all tables to be added here
-    table_list.append(master)
+    table_list = [
+        master,
+        batting,
+    ]
 
     ###
     parser = argparse.ArgumentParser()
@@ -63,7 +92,7 @@ if __name__ == "__main__":
 
     if args.create_tables == "Y":
         for table in table_list:
-            table.create(engine)
+            table.create(engine, checkfirst=True)
 
     if args.drop_tables == "Y":
         for table in table_list:
